672fd0f87da7e9bda5e1f5d839c599d4733736a1
Matthew Ao <37258460+aoruize@users.noreply.github.com>
2019-08-05 19:23:26 -0400

    Create Output1.java

---
+++ outputs/Output1.java
@@ -0,0 +1,11 @@
+/* Output1.java
+ * This program outputs "Hello World!"
+ * Author: Matthew Ao
+ * 06 February 2019
+ */
+
+class Output1{
+  public static void main(String[] args){
+    System.out.println("Hello World!"); // Outputs "Hello World!"
+  }
+}
