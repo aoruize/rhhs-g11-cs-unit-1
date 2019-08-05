cb3012cf3934ee57417ca136f16e2f7e14aa2af7
Matthew Ao <37258460+aoruize@users.noreply.github.com>
2019-08-05 19:24:29 -0400

    Add files via upload

---
+++ outputs/Output6.java
@@ -0,0 +1,11 @@
+/* Output6.java
+ * This program outputs the remainder of 5/2
+ * Author: Matthew Ao
+ * 06 February 2019
+ */
+
+class Output6{
+  public static void main(String[] args){
+    System.out.println("The remainder of 5 / 2 =" + "" + 5%2); // Outputs the remainder of 5/2
+  }
+}
