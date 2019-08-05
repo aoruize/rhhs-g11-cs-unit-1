aa7e459defec1f11fef7c8b8ccd6c3f70bce29a5
Matthew Ao <37258460+aoruize@users.noreply.github.com>
2019-08-05 19:23:38 -0400

    Add files via upload

---
+++ outputs/Output2.java
@@ -0,0 +1,11 @@
+/* Output2.java
+ * This program outputs a sequence of numbers
+ * Author: Matthew Ao
+ * 06 February 2019
+ */
+
+class Output2{
+  public static void main(String[] args){
+    System.out.println("12\n13\n14\n15"); // Outputs a sequence of numbers
+  }
+}
