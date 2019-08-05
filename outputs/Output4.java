e798cde7179441da23d8b18213e890ed0425ec9a
Matthew Ao <37258460+aoruize@users.noreply.github.com>
2019-08-05 19:24:03 -0400

    Add files via upload

---
+++ outputs/Output4.java
@@ -0,0 +1,11 @@
+/* Output4.java
+ * This program outputs a concatenated sentence
+ * Author: Matthew Ao
+ * 06 February 2019
+ */
+
+class Output4{
+  public static void main(String[] args){
+    System.out.println("The area code for Toronto is" + " 416"); // Outputs a message
+  }
+}
