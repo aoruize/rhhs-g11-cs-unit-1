a04970d09027995e30ef8168758d4dc24de16b6a
Matthew Ao <37258460+aoruize@users.noreply.github.com>
2019-08-05 19:24:16 -0400

    Add files via upload

---
+++ outputs/Output5.java
@@ -0,0 +1,11 @@
+/* Output5.java
+ * This program outputs the product of 3 and 4
+ * Author: Matthew Ao
+ * 06 February 2019
+ */
+
+class Output5{
+  public static void main(String[] args){
+    System.out.println(3*4); // Outputs the product of 3 and 4
+  }
+}
