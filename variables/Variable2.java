5769ca297a0e4800386b3ab9b197b11ca016fea8
Matthew Ao <37258460+aoruize@users.noreply.github.com>
2019-08-05 19:10:17 -0400

    Add files via upload

---
+++ variables/Variable2.java
@@ -0,0 +1,13 @@
+/* Variable2.java
+ * This program outputs the product of two integer variables
+ * Author: Matthew Ao
+ * 06 February 2019
+ */
+
+class Variable2{
+  public static void main(String[] args){
+    byte num1; byte num2;
+    num1 = 10; num2 = 20;
+    System.out.println(num1*num2); // outputs product of num1 and num2
+  }
+}
