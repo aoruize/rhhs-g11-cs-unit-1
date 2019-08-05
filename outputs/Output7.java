326903c7a913f50b4b485768ba1d263133ce9e3c
Matthew Ao <37258460+aoruize@users.noreply.github.com>
2019-08-05 19:24:41 -0400

    Add files via upload

---
+++ outputs/Output7.java
@@ -0,0 +1,16 @@
+/* Output7.java
+ * This program prints "Hello" several times
+ * Author: Matthew Ao
+ * 06 February 2019
+ */
+
+class Output7{
+  public static void main(String[] Args){
+    System.out.println("Hello");
+    System.out.println("Hello");
+    System.out.println("Hello");
+    System.out.println("Hello");
+    System.out.println("Hello");
+    System.out.println("\nHello\nHello\nHello\nHello\nHello");
+  }
+}
