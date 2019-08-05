09ed093a3ddcac06bb40ddb80f578c46d1285304
Matthew Ao <37258460+aoruize@users.noreply.github.com>
2019-08-05 19:09:54 -0400

    Create variable1.java

---
+++ variables/variable1.java
@@ -0,0 +1,13 @@
+/* Variable1.java
+ * This program outputs an integer variable
+ * Author: Matthew Ao
+ * 06 February 2019
+ */
+
+class Variable1{
+  public static void main(String[] args){
+    byte num;
+    num = 5;
+    System.out.println(num); // Outputs num variable
+  }
+}
