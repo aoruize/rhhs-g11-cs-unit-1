9ed339e72aba5e11411fc7bbe9350e84da64e7b1
Matthew Ao <37258460+aoruize@users.noreply.github.com>
2019-08-05 19:23:50 -0400

    Add files via upload

---
+++ outputs/Output3.java
@@ -0,0 +1,11 @@
+/* Output3.java
+ * This program outputs my first and last name
+ * Author: Matthew Ao
+ * 06 February 2019
+ */
+
+class Output3{
+  public static void main(String[] args){
+    System.out.println("Matthew Ao"); // Outputs "Matthew Ao"
+  }
+}
